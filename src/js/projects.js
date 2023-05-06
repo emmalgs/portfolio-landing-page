d1f6157573a40b2160ce7e183835142564154899
Emma Gerigscott <elgerig@gmail.com>
2023-05-05 20:18:47 -0700

    add gifs to project cards and adjust main-container in styles

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,6 +1,8 @@
 import artInspo from "./../assets/images/art-inspo.gif";
 import planetAger from "./../assets/images/planet-age.gif";
 import prioritime from "./../assets/images/prioritime2.gif";
+import currency from "./../assets/images/currency.gif";
+import pizza from "./../assets/images/pizza.gif";
 
 export default function renderProjects(){
   const nav = document.querySelector("#nav");
@@ -19,6 +21,8 @@ export default function renderProjects(){
   createCards(main, "Art Inspirer", artInspo, "https://github.com/emmalgs/art-inspiration", "Enter a 'vibe' to see related art from the Art Institute of Chicago");
   createCards(main, "Planetary Ager", planetAger, "https://github.com/emmalgs/space-age-calculator", "Type in your age to see how old you are on other planets");
   createCards(main, "PrioriTime", prioritime, "https://github.com/emmalgs/prioritime", "Epicodus JavaScript Team Week Project");
+  createCards(main, "Currency Exchange", currency, "https://github.com/emmalgs/currency-exchange", "Currency exchange utilizing APIs to access up-to-date global exchange rates");
+  createCards(main, "Bucky's One Stop Pizza Shop", pizza, "https://github.com/emmalgs/make-a-pizza", "Build a pizza and add it to your order. Be warned, the chef is indeed a guinea pig.");
 }
 
 function createCards(mainDiv, title, imageSrc, link, description) {
